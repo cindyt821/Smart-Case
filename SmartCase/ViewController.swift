5c5c81df4c4280ed39ccafa4a40b1a274f6b7cda
Cindy Tang <cindyt821@gmail.com>
2021-01-21 13:21:20 -0800

    Initial Commit

---
+++ SmartCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCase
+//
+//  Created by CINDY TANG on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
