41fcb287a0f1da0b9f3fca8ffe9d1e76cbe69431
Cindy Tang <cindyt821@gmail.com>
2021-01-22 12:55:09 -0800

    initial xcode project upload

--- SmartCase/ViewController.swift
+++ SmartCase/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SignInFacebook: UIButton!
+    @IBOutlet weak var SignInGoogle: UIButton!
+    @IBOutlet weak var SignInEmail: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func SignInEmail(_ sender: Any) {
+        print("sign with email")
+    }
+    
 }
 
